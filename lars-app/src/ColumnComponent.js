2914189bce3ddc52c1f9512b2e6f10859bf5f3e5
lars-sanhudo <sanhudo.lars@gmail.com>
2022-12-24 02:30:26 -0300

    Correct some bugs.

--- lars-app/src/ColumnComponent.js
+++ lars-app/src/ColumnComponent.js
@@ -1,15 +1,26 @@
 import ItemComponent from "./ItemComponent";
 import data from "./dados.json";
 
+
+
+function verificated(args) {
+    if(args == 1){
+        return (data.word1.left);
+    }else {
+        return "Don't find anything."
+    }
+}
+
+
 function ColumnComponent(){
     
-    var [] = numbers;
+    //var [] = numbers;
     
     return(
         <>
         <div className="columnClass">
         <ItemComponent
-        content = {data.word1.left}
+        content = {verificated(data.word1.index)}
         position = "1"
         
         />
@@ -31,9 +42,8 @@ function ColumnComponent(){
 }
 
 
-function Random(numbers) {
-    let numbers  = this.numbers;
-}
+
+
 
 
 
