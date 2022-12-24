43a2a65f4249c85b12dd908de07e28d4519f5448
lars-sanhudo <sanhudo.lars@gmail.com>
2022-12-24 14:03:38 -0300

    Add random selection of the words and meanings.

--- lars-app/src/ColumnComponent.js
+++ lars-app/src/ColumnComponent.js
@@ -1,15 +1,60 @@
 import ItemComponent from "./ItemComponent";
-import data from "./dados.json";
+import dadosArray from "./DadosArray";
 
 
 
-function verificated(args) {
-    if(args == 1){
-        return (data.word1.left);
-    }else {
-        return "Don't find anything."
+let indexPosition = RandomPosition();
+
+function RandomPosition() {
+
+    let i = 0;
+    let indexControl = [[0,0],[0,0],[0,0],[0,0]];
+    let ok = false;
+    //console.log("Entrei aqui");
+    console.log("///////////////////////////////////////");
+
+    while(i < indexControl.length) {
+        
+        console.log("Entrei aqui");
+        //console.log(indexControl.length);
+        
+        if(indexControl[i][0] == 0){
+            indexControl[i][0] = Math.floor(Math.random()*7+1);
+            console.log("Random Position: " + indexControl[i][0]);
+
+            //console.log("indexControl: "+indexControl);
+            while(ok === false){
+                let randomNumber = Math.floor(Math.random()*indexControl.length);
+                console.log("RandomNumber: " + randomNumber);
+                //console.log(randomNumber);
+                if(indexControl[randomNumber][1] == 0){
+                    indexControl[randomNumber][1] = indexControl[i][0];
+                    ok = true;
+                    //console.log("Entrei aqui");
+                    //console.log(indexControl);
+                    i++;
+                }
+                
+            }
+    
+            ok = false;
+            
+        }
+
+       
+        //console.log(indexControl);
+        
     }
+
+    return (indexControl);
 }
+
+
+
+console.log(indexPosition);
+
+
+
 
 
 function ColumnComponent(){
@@ -20,7 +65,7 @@ function ColumnComponent(){
         <>
         <div className="columnClass">
         <ItemComponent
-        content = {verificated(data.word1.index)}
+        content = {"I'm a content."}
         position = "1"
         
         />
---
+++ lars-app/src/DadosArray.js
@@ -0,0 +1,21 @@
+const data  = [
+    [
+        "word1", "meaning1"
+    ],
+    [
+        "word2", "meaning2"
+    ],
+    [
+        "word3", "meaning3"
+    ],
+    [
+        "word4", "meaning4"
+    ],
+    [
+        "word5", "meaning5"
+    ],
+    [
+        "word6", "meaning6"
+    ]
+]
+
